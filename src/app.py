5d58021641988fea8d5757239334dea1ec218065
Gustavo <gustavo.souza@bi4yourdata.com>
2024-09-22 16:47:49 -0300

    feat: validador de Excel funcionando

--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
 from frontend import ExcelValidadorUI
+from backend import process_excel
+
+
 
 
 def main():
     ui = ExcelValidadorUI()
     ui.display_header()
+
+    upload_file = ui.upload_file()
+
+    if upload_file:
+        df, result, error = process_excel(upload_file)
+        ui.display_results(result, error)
 
 if __name__ == "__main__":
     main()
